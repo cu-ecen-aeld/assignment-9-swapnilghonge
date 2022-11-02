e017b1bf39809c141a92e54c587f54eac3a76374
swapnilghonge <swapnil.ghonge@gmail.com>
2022-11-01 22:55:47 -0600

    A9 changes

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = f703a3c4b2b0aba0f73f4b81bf84e2f267a7735c
+AESD_ASSIGNMENTS_VERSION = 55e3ce4c5b75e8016dce567501c4e09dfcdbedd3
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -34,6 +34,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar.ko $(TARGET_DIR)/usr/bin
 	
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
